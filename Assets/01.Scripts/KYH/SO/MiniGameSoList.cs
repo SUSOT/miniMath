82ed5453076db973b470a7ce1354c2bc54363794
fishmaster <a01022639410@gmail.com>
2024-09-06 01:50:30 +0900

    feat : SOList

---
+++ Assets/01.Scripts/KYH/SO/MiniGameSoList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/MiniGameSOList")]
+public class MiniGameSoList : ScriptableObject
+{
+    public MiniGameSO gameSO;
+}
