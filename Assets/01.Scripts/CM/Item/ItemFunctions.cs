f346eaeb7caeaebfd8c46a01dc09032bffb5f83f
SUSOT <a01076754042@gmail.com>
2024-09-26 09:39:32 +0900

    AddTimeLineAndScripts

---
+++ Assets/01.Scripts/CM/Item/ItemFunctions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFunctions : MonoBehaviour
+{
+    public void MyScoreUp()
+    {
+
+    }
+    public void MyScoreDown()
+    {
+
+    }
+    public void AnotherScoreUp()
+    {
+
+    }public void AnotherScoreDown()
+    {
+
+    }
+    public void EveryScoreUp()
+    {
+
+    }
+    public void EveryScoreDown()
+    {
+
+    }
+    public void DoubleScore()
+    {
+
+    }
+}
