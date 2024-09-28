bea8a67cfe043e5a3e9f1b59be801c47fc4b1abc
SUSOT <a01076754042@gmail.com>
2024-09-29 01:10:25 +0900

    CreateTutorial

---
+++ Assets/01.Scripts/CM/Tutorial/ChangeTutorial.cs
@@ -0,0 +1,34 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ChangeTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject _tutorial;
+    [SerializeField] private int _maxValue;
+    [SerializeField] private int _moveValue;
+    private int _curValue = 0;
+
+    public void RightMove()
+    {
+        if (_curValue < _maxValue)
+        {
+            _tutorial.transform.DOMoveX(_tutorial.transform.position.x -_moveValue, 0.5f);
+            _curValue++;
+            Debug.Log("r");
+        }
+    }
+    public void LeftMove()
+    {
+        if (_maxValue >= _curValue && _curValue > 0)
+        {
+            _tutorial.transform.DOMoveX(_tutorial.transform.position.x + _moveValue, 0.5f);
+            _curValue--;
+            Debug.Log("l");
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(_maxValue >= _curValue && _curValue > 0);
+    }
+}
