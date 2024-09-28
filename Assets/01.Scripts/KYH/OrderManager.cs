ae43efe2834dddc5cbbb39e9d7d539c02a8961d6
fishmaster <a01022639410@gmail.com>
2024-09-28 20:05:47 +0900

    fix : TeamCardSpacing

--- Assets/01.Scripts/KYH/OrderManager.cs
+++ Assets/01.Scripts/KYH/OrderManager.cs
@@ -25,61 +25,61 @@ public class OrderManager : MonoBehaviour
     private TextMeshProUGUI mainText;
 
 
-    private void Start()
-    {
-        playerCard = GameManager.Instance.playerSO;
-        maxTeamCount = GameManager.Instance.teamCount;
-        StartSolve();
-    }
+    //private void Start()
+    //{
+    //    playerCard = GameManager.Instance.playerSO;
+    //    maxTeamCount = GameManager.Instance.teamCount;
+    //    StartSolve();
+    //}
 
-    public void SolveStart()
-    {
-        GetProblem();
-        countDownTimer.gameObject.SetActive(true);
-        countDownTimer.TimerStart();
-    }
+    //public void SolveStart()
+    //{
+    //    GetProblem();
+    //    countDownTimer.gameObject.SetActive(true);
+    //    countDownTimer.TimerStart();
+    //}
 
-    public void SolveEnd() // 타이머 끝나면
-    {
-        countDownTimer.gameObject.SetActive(false);
-        AnswerStartAnimation();
-        teamCount = 1;
-        OrderChange();
-    }
+    //public void SolveEnd() // 타이머 끝나면
+    //{
+    //    countDownTimer.gameObject.SetActive(false);
+    //    AnswerStartAnimation();
+    //    teamCount = 1;
+    //    OrderChange();
+    //}
 
-    private void AnswerStartAnimation()
-    {
+    //private void AnswerStartAnimation()
+    //{
 
-    }
+    //}
 
-    private void OrderChange()
-    {
-        currentProblem = playerCard[teamCount];
-        { 팀이름 텍스트 바꿔주기}
-        선택 차례
-        문제 선택 타이머 시작
-    }
+    //private void OrderChange()
+    //{
+    //    currentProblem = playerCard[teamCount];
+    //    { 팀이름 텍스트 바꿔주기}
+    //    선택 차례
+    //    문제 선택 타이머 시작
+    //}
 
-    private void GetProblem()
-    {
-        int rand = Random.Range(0, problemList.problemSO.Count);
-        currentProblem = problemList.problemSO[rand];
-        //문제 세팅
-        mainText.text = "어떤 것이 정답일까요??";
-    }
+    //private void GetProblem()
+    //{
+    //    int rand = Random.Range(0, problemList.problemSO.Count);
+    //    currentProblem = problemList.problemSO[rand];
+    //    //문제 세팅
+    //    mainText.text = "어떤 것이 정답일까요??";
+    //}
 
-    private void TimerEnd()
-    {
-        WrongAnswer();
-    }
+    //private void TimerEnd()
+    //{
+    //    WrongAnswer();
+    //}
 
-    public void WrongAnswer()
-    {
+    //public void WrongAnswer()
+    //{
 
-    }
+    //}
 
-    public void CorrectAnswer()
-    {
+    //public void CorrectAnswer()
+    //{
 
-    }
+    //}
 }
