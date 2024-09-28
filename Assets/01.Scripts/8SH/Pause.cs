3d659772701804d32d18ade3725c8ca527eeceab
8Sehyun <8Sehyun@gmail.com>
2024-09-28 22:52:37 +0900

    Added Pause

---
+++ Assets/01.Scripts/8SH/Pause.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool opened = false;
+    private CanvasGroup pause;
+
+    private void Awake()
+    {
+        pause = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (opened)
+            {
+                opened = false;
+                pause.alpha = 0;
+                pause.blocksRaycasts = false;
+                Time.timeScale = 1;
+            }
+            else
+            {
+                opened = true;
+                pause.alpha = 1;
+                pause.blocksRaycasts = true;
+                Time.timeScale = 0;
+
+            }
+        }
+    }
+}
