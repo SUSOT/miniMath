93a20a22fe0191b3e32ca3802cc3f7fd6bccc880
fishmaster <a01022639410@gmail.com>
2024-09-29 17:43:20 +0900

    Revert "Merge branch 'KYH' into Base"
    This reverts commit d0ec03d98a1a4fb9e2d3948dacdf909191a2e23b, reversing
    changes made to 8628d20791fe530fdb0adb8fa3b44c7b676945e7.

--- Assets/01.Scripts/CM/OpenChestTimeLine.cs
+++ Assets/01.Scripts/CM/OpenChestTimeLine.cs
@@ -7,49 +7,17 @@ using UnityEngine.UI;
 public class OpenChestTimeLine : MonoBehaviour
 {
     public PlayableDirector currentCutScene;
-    //진짜 살면서 제일 짜칩니다
-    //private GameObject[] answerPoint;
 
-    public PlayableAsset leftAnswer;
-    public PlayableAsset rightAnswer;
-    public PlayableAsset middleAnswer;
+    public PlayableAsset firstAnswer;
+    public PlayableAsset secondAnswer;
+    public PlayableAsset thirdAnswer;
 
-    private Correct correct;
-
-    private int pointPos;
-
-    private List<GameObject> answerPoint = new List<GameObject>();
-
-    public void TimeLine()//짜칩니다
+    private void OnEnable()
     {
-        OrderManager orderManager = FindAnyObjectByType<OrderManager>();
-        foreach(Transform answerPoints in orderManager.randAnswerButtonPos)
-        {
-            answerPoint.Add(answerPoints.gameObject);
-        }
-
-        for (int i=0; i<answerPoint.Count; i++)
-        {
-           correct = answerPoint[i]?.GetComponentInChildren<Correct>();
-            print(correct);
-            if (correct != null)
-            {
-                pointPos = answerPoint[i].GetComponent<ProblemPoint>().point;
-            }
-            else
-                print(answerPoint[i]+"얘는 없음");
-        }
+        //GameManager.Instance
         currentCutScene = GetComponent<PlayableDirector>();
-
-        if (pointPos == 1)
-            currentCutScene.playableAsset = middleAnswer;
-        else if (pointPos == 2)
-            currentCutScene.playableAsset = leftAnswer;
-        else if (pointPos == 3)
-            currentCutScene.playableAsset = rightAnswer;
-
-
-        orderManager.answerRevealed = currentCutScene;
+        currentCutScene.playableAsset = firstAnswer;
+        FindAnyObjectByType<OrderManager>().answerRevealed = currentCutScene;
     }
 
     //private void Start()
